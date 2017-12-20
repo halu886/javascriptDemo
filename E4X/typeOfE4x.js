6327742ac9e2e43a7a365f8f27d4b901e15e27bc
halu <halu886@qq.com>
2017-12-20 21:10:00 +0800

    XML

---
+++ E4X/typeOfE4x.js
@@ -0,0 +1,17 @@
+// var x = new Xml(
+//     "<employee position=\"software Engineer\"><name>halu"
+//     + "886</name></employee>"
+// );
+
+
+// var employee = <employee position="Software Engineer">
+//     <name>Nicholas C. Zakas</name>
+// </employee>;
+
+// var data = <name>Nicholas
+// var employee = <employee position="Software Engineer"><name>halu886</name></employee>;
+// console.log(employee.toString())
+
+var data = new XML("<name>halu 886</name>")
+console.log(data.toString());
+console.log(data.toXMLString())
