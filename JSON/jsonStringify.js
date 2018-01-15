95db9ff4a3dba093490019b18339311e207576e9
halu <halu886@qq.com>
2018-01-15 09:23:52 +0800

    finish JSON chapter

--- JSON/jsonStringify.js
+++ JSON/jsonStringify.js
@@ -22,5 +22,33 @@ var book = {
 
 // var jsonText = JSON.stringify(book, null, 4)
 
-var jsonText = JSON.stringify(book, null, "--------------")
-console.log(jsonText)
+// var jsonText = JSON.stringify(book, null, "--------------")
+// console.log(jsonText)
+
+// console.log((new Date()).toISOString(), (new Date()).toJSON())
+
+var book = {
+    title: "Professional JavaScript",
+    "authors": [
+        "Nicholas C. Zakas"
+    ],
+    edition: 3,
+    year: 2011,
+    // toJSON: function () {
+    //     return this.title;
+    // }
+    releaseDate: new Date(2011, 11, 1)
+}
+
+var jsonText = JSON.stringify(book)
+// console.log(jsonText)
+
+var bookCopy = JSON.parse(jsonText, function (key, value) {
+    if (key == "releaseDate") {
+        return new Date(value);
+    } else {
+        return value
+    }
+})
+
+console.log(jsonText, bookCopy, bookCopy.releaseDate.getFullYear())
