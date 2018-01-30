897108bb624d2c452395b6b7174ea438940eb7a5
halu <halu886@qq.com>
2018-01-30 09:15:06 +0800

    高级技巧

--- AdvancedTechniques/index.js
+++ AdvancedTechniques/index.js
@@ -9,7 +9,32 @@ function Person(name, age, job) {
 }
 var person = Person("halu", 21, "Software Engineer")
 
-console.log(window.name);
-console.log(person.name)
+// console.log(window.name);
+// console.log(person.name)
 // console.log(window.age);
 // console.log(window.job);
+
+function Polygon(sides) {
+    if (this instanceof Polygon) {
+        this.sides = sides;
+        this.getArea = function () {
+            return 0;
+        }
+    } else {
+        return new Polygon(sides);
+    }
+}
+
+function Rectangle(width, height) {
+    Polygon.call(this, 2);
+    this.width = width;
+    this.height = height;
+    this.getArea = function () {
+        return this.width * this.height;
+    }
+}
+
+Rectangle.prototype = new Polygon();
+
+var rect = new Rectangle(5, 10);
+console.log(rect.sides);
